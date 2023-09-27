2fd711e41a1c49a0af66c35812ef342b1a584f65
Donald French <dhf0820@gmail.com>
2023-09-27 08:42:38 -0700

    Updated version

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ var Mode string
 var Env string
 
 func main() {
-	version = "230926.0"
+	version = "230927.0"
 	log.SetDebuglevel("DEBUG3")
 	log.Info("run mode: " + os.Getenv("MODE"))
 	switch os.Getenv("MODE") {
